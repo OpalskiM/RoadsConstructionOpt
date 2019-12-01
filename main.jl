44014e919f608ebb3add3996114fb3ba7e3438e5
Marcin Opalski <32035902+OpalskiM@users.noreply.github.com>
2019-12-01 21:03:53 +0100

    Update main.jl

--- main.jl
+++ main.jl
@@ -15,18 +15,17 @@ using RoadsConstructionOpt
 #
 map_file_path = joinpath(".","winnipeg_downtownf.osm")
 #Example
-N = 100;
-l = 5.0;
+N = 100; #Number of agents
+l = 5.0; # Vehicle length
 map_data = OpenStreetMapX.get_map_data(map_file_path);
 sim_data = OpenStreetMapXDES.get_sim_data(map_data, N, l);
 
 
-λ_ind = 0.4
-#λ_soc = 1.0
-routes = map_data.e;
-no_solutions = 10
-roadwork_time = 10
-no_of_partitions = 5
+λ_ind = 0.4 #Learning rate
+routes = map_data.e;    #Routes to renovate
+no_solutions = 10    #Number of solutions in evolutionary algorithm
+roadwork_time = 10  # approximate length of roadwork of single segment  
+no_of_partitions = 5   #number of steps in roadwork process
 crossover_rate = 0.8
 mutation_rate = 0.2
 elitism = 0.1
