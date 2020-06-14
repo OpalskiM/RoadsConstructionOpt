27d40cdb376887eb2ba777f684333032406730dc
Marcin Opalski <32035902+OpalskiM@users.noreply.github.com>
2020-06-14 21:55:49 +0200

    Update simulation_functions .jl

--- src/simulation_functions .jl
+++ src/simulation_functions .jl
@@ -141,35 +141,3 @@ function unclog!(sim_data::OpenStreetMapXDES.SimData, sim_flow::ControlFlow,
         end
     end
 end
-
-###functions used in optimising roadworks
-
-function split_sequence(edges::Vector{Tuple{Int,Int}}, k::Int)
-    edgs = deepcopy(edges)
-    seq = Vector{Tuple{Int,Int}}[]
-    size = Int(round(length(edgs)/k))
-    len = length(edgs)
-    while len > 0
-        push!(seq,splice!(edgs,1:min(size, length(edgs))))
-        len -= size
-    end
-    seq
-end
-
-function calculate_fitness(times, reference)
-    times = filter!(x -> !isnan(x), times ./reference)
-    sum(times)/length(times)
-end
-
-#function to assess fitness of solution with removed edges from the graph
-function get_solution(sim_data::OpenStreetMapXDES.SimData,routes::Vector{Tuple{Int,Int}},
-                    reference_times::Vector{Float64},
-                     roadwork_time::Int, no_of_partitions::Int)
-    res = zeros(length(sim_data.population))
-    for segments in split_sequence(routes,no_of_partitions)
-        g = remove_edges(sim_data.map_data, segments)
-        res += run_sim!(sim_data, roadwork_time, g)
-    end
-    res /= no_of_partitions
-    (calculate_fitness(res,reference_times),routes)
-end
