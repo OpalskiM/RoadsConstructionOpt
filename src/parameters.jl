a0d95e955118e32fe1556bd1353b6c0a3004589a
Marcin Opalski <32035902+OpalskiM@users.noreply.github.com>
2020-06-14 20:21:39 +0200

    Update parameters.jl

--- src/parameters.jl
+++ src/parameters.jl
@@ -5,6 +5,6 @@ using Parameters
            l::Float64 = 5.0 #vehicle length
        ##    roadwork_time::Int = 1  # approximate length of roadwork of single segment
       ##     no_of_partitions::Int = 5 # #number of steps in roadwork process
-           n::Int = 10 # number of roads to be removed from the graph
+      ##     n::Int = 10 # number of roads to be removed from the graph
        end
        ModelSettings
