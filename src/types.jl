bf05bafebda75fbe386d96cd8f78e450ee9d890c
Marcin Opalski <32035902+OpalskiM@users.noreply.github.com>
2020-06-17 23:52:25 +0200

    Update types.jl

--- src/types.jl
+++ src/types.jl
@@ -1,30 +1,22 @@
 mutable struct Agent
-    start_node::Int64
-    fin_node::Int64
-    route::Array{Tuple{Int64,Int64},1}
-    current_edge::Int64
+    	start_node::Int64
+   	fin_node::Int64
+    	route::Array{Tuple{Int64,Int64},1}
+    	current_edge::Int64
 end
 
 mutable struct SimData
-    map_data::OpenStreetMapX.MapData
+        map_data::OpenStreetMapX.MapData
 	driving_times::SparseArrays.SparseMatrixCSC{Float64,Int64}
 	velocities::SparseArrays.SparseMatrixCSC{Float64,Int64}
 	max_densities::SparseArrays.SparseMatrixCSC{Float64,Int64}
 	population::Vector{Agent}
 end
 
-mutable struct FlowData
-    map_data::OpenStreetMapX.MapData
-    DAs_to_intersection::Dict{Int64,Int64}
-    demographic_data::Dict{Int64, Int64}
-    flow_dictionary::Dict{Int64,Int64}
-    flow_matrix::SparseArrays.SparseMatrixCSC{Int64,Int64}
-end
-
-
 mutable struct Stats
-	delays::Array{Float64,1}
-    cars_count::SparseArrays.SparseMatrixCSC{Float64,Int64}
-    avg_driving_times::SparseArrays.SparseMatrixCSC{Float64,Int64}
+	cars_count::SparseArrays.SparseMatrixCSC{Float64,Int64}
+	avg_driving_times::SparseArrays.SparseMatrixCSC{Float64,Int64}
+	actual_driving_times::SparseArrays.SparseMatrixCSC{Float64,Int64} #latest travel time on the edge.
 end
 
+Stats(m::Int,n::Int) = Stats(SparseArrays.spzeros(m, n), SparseArrays.spzeros(m, n),SparseArrays.spzeros(m, n))
