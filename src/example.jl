73f518e661843b8b95ce334c946a219c30c4fe56
Marcin Opalski <32035902+OpalskiM@users.noreply.github.com>
2020-06-18 17:00:00 +0200

    Update example.jl

--- src/example.jl
+++ src/example.jl
@@ -12,10 +12,10 @@ using Parameters
 
 include("types.jl")
 include("simulation_functions.jl")
-include("simulator.jl")
 include("parameters.jl")
 
-const p = ModelSettings(10000,20,5.0)
+const p = ModelSettings(10000,5.0)
+#N = 10000, l = 5.0
 
 pth = joinpath(dirname(pathof(OpenStreetMapX)),"..","test","data")###exemplary map
 name = "reno_east3.osm"
@@ -23,12 +23,5 @@ name = "reno_east3.osm"
 map_data =  OpenStreetMapX.get_map_data(pth,name,use_cache = false);
 sim_data = get_sim_data(map_data,N,l)
 
-#get reference scenario, with no roadworks
-reference_times = run_sim!(deepcopy(sim_data), iter)
-
-m=deepcopy(map_data) #creating a copy of map_data
-s=deepcopy(sim_data) #creating a copy of sim_data
-include("Removing_edges.jl")
-
-#creating solution for scenario with roadworks
-@time Solution = run_sim!(s,iter)
+include("simulator.jl")
+run_simulation!(sim_data)
