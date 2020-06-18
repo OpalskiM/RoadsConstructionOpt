7de7f73edae4850ca89fd3f0ddfa72a1f7c9eccf
Marcin Opalski <32035902+OpalskiM@users.noreply.github.com>
2020-06-18 16:57:58 +0200

    Update parameters.jl

--- src/parameters.jl
+++ src/parameters.jl
@@ -1,9 +1,5 @@
 using Parameters
 @with_kw struct ModelSettings
-    N::Int = 1000 #number of agents
-    iter::Int = 20 #number of iterations
+    N::Int = 10000 #number of agents
     l::Float64 = 5.0 #vehicle length
-    ##    roadwork_time::Int = 1  # approximate length of roadwork of single segment
-    ##     no_of_partitions::Int = 5 # #number of steps in roadwork process
-    ##     n::Int = 10 # number of roads to be removed from the graph
 end
