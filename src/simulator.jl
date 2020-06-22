0d7f7b7a3e7b16d0f209377efae93e59968b9c53
Marcin Opalski <32035902+OpalskiM@users.noreply.github.com>
2020-06-22 11:23:08 +0200

    Update simulator.jl

--- src/simulator.jl
+++ src/simulator.jl
@@ -1,16 +1,17 @@
-function run_simulation!(sim::SimData, mapa::OpenStreetMapX.OSMData,speeds = OpenStreetMapX.SPEED_ROADS_URBAN)
+function run_simulation!(sim::SimData, mapa::OpenStreetMapX.OSMData,speeds = OpenStreetMapX.SPEED_ROADS_URBAN) #OSM object used only for visualisation
     m, n = size(sim.map_data.w)
     stats = Stats(m, n)
-    stats.vehicle_load = SparseArrays.spzeros(m, n) #1. SparseArray vehicle_load z obciazeniami samochodow na lukach
-	stats.actual_driving_times=deepcopy(sim.driving_times) #3. creating deepcopy
+    stats.vehicle_load = SparseArrays.spzeros(m, n) #number of agents on edges
+	stats.actual_driving_times=deepcopy(sim.driving_times) #driving times with 0 traffic
     stats.simulation_total_time = 0.0 #summing total time of all agents in the simulation
 	for agent in sim.population
-	    agent.route = get_route(sim.map_data, stats.actual_driving_times, agent.start_node,agent.fin_node)  #4 Agent jedzie najszybsza trasa korzystajac z actual_driving_times
+	    agent.route = get_route(sim.map_data, stats.actual_driving_times, agent.start_node,agent.fin_node) #Each agent prefers the fastest route using actual driving times 
 		for i in 1:length(agent.route)
-		    update_stats!(stats, agent.route[i][1], agent.route[i][2]) #5 i 6 aktualizacja vehicle_load oraz stats.actual_driving_times za pomocą funkcji update_stats!
-		 end
-	end #7. looping
-    cars_per_edge = stats.vehicle_load
+		    update_stats!(stats, agent.route[i][1], agent.route[i][2]) #stats.vehicle_load and stats.actual_driving_times updated with the route chosen by agent
+		end
+	end 
+	#Visualization
+    cars_per_edge = stats.vehicle_load 
     mData = deepcopy(sim.map_data)
     plot_edge_load(mapa,mData,cars_per_edge)
 end
