f26e8cd6415a837b250119b7339db939a74b3292
Marcin Opalski <32035902+OpalskiM@users.noreply.github.com>
2020-08-30 22:13:42 +0200

    Update RoadsConstructionOpt.jl

--- src/RoadsConstructionOpt.jl
+++ src/RoadsConstructionOpt.jl
@@ -23,6 +23,8 @@ include("visuals.jl")
 include("removed_edges.jl")
 include("roadworks.jl")
 include("roadworks_opt.jl")
+include("rand_sol.jl")
+include("optimal_visual.jl")
 
 export get_sim, run_simulation
 export ModelSettings, Agent, Stats, SimData
@@ -30,6 +32,8 @@ export plot_edge_load, plot_edge_load_removed
 export top_congested_roads
 export get_solution, split_sequence, remove_edges
 export opt, f
+export rand_perm, rand_sol
+export plot_optimal_plan, plot_random_plan
 
 
 end # module
